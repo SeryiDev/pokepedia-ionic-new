fb901c12dbe533c3e67a27cb38ae3e2b0e4f8867
Sergio García <sergicodcraft@gmail.com>
2022-10-04 10:05:32 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pokepedia-ionic-new',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
